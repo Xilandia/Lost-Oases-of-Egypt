39d0948666fc9844c0407ad169a8072cc713ad4d
Omer Segal <omer_segal@live.com>
2023-06-11 17:16:50 +0300

    Added wave system
    Implemented wave system (tested).

---
+++ InGame Final/Assets/_Scripts/Utility/Scriptable/EnemyStage.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Scripts.Utility.Scriptable
+{
+    [CreateAssetMenu(fileName = "New Stage", menuName = "Enemy Spawning/Stage")]
+    public class EnemyStage : ScriptableObject
+    {
+        public List<EnemyWave> waves;
+        public float waveLength;
+    }
+}
